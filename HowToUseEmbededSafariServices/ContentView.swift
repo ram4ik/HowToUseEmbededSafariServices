5fbfd72820bfdec29438288e09d4182c59bc2303
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-11 10:15:03 +0200

    init

--- HowToUseEmbededSafariServices/ContentView.swift
+++ HowToUseEmbededSafariServices/ContentView.swift
@@ -7,15 +7,40 @@
 //
 
 import SwiftUI
+import SafariServices
 
 struct ContentView: View {
+    
+    @State var show = false
+    
     var body: some View {
-        Text("Hello World")
+        Button(action: {
+            self.show.toggle()
+        }) {
+            Text("Safari Services").padding().foregroundColor(.white)
+        }.background(LinearGradient(gradient: .init(colors: [.orange, .red]), startPoint: .leading, endPoint: .trailing))
+        .cornerRadius(20)
+            .sheet(isPresented: $show) {
+                safari()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct safari: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<safari>) -> SFSafariViewController {
+        
+        let controller = SFSafariViewController(url: URL(string: "https://www.youtube.com/watch?v=FvVnP8G6ITs")!)
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<safari>) {
+        
     }
 }
